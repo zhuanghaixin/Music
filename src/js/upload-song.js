24555c4ca5473d00eef4999a95eb9be105bf9018
zhuanghaixin <649308293@qq.com>
2019-07-18 10:22:38 +0800

    [Music] 网易云首页 add index.html,home.css,reset,css

--- src/js/upload-song.js
+++ src/js/upload-song.js
@@ -1,3 +1,4 @@
+
 {
     let view={
         el:'.uploadArea',
@@ -5,7 +6,11 @@
             return $(this.el).find(selector)[0]  //jquery获取对象的一个元素才是dom
         }
     }
-    let model={}
+    let model={
+        data:{
+            status:'open'
+        }
+    }
     let controller={
         init(view,model){
 
@@ -28,23 +33,31 @@
                 drop_element: this.view.find('#uploadContainer'),        //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
                 auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传
                 init: {
-                    'FilesAdded': function (up, files) {
+                    'FilesAdded':(up, files)=>{
                         plupload.each(files, function (file) {
                             // 文件添加进队列后,处理相关的事情
                         });
                     },
-                    'BeforeUpload': function (up, file) {
+                    'BeforeUpload':  (up, file)=>{
                         // 每个文件上传前,处理相关的事情
                         window.eventHub.emit('beforeUpload')
+                        console.log('xxxx1222333')
+                        console.log(this)
+                        if(this.model.data.status==='closed'){
+                            return false
+                        }else{
+                            this.model.data.status='closed'
+                            return true
+                        }
                     },
-                    'UploadProgress': function (up, file) {
+                    'UploadProgress': (up, file)=>{
                         // 每个文件上传时,处理相关的事情
                         uploadStatus.textContent = '上传中'
                     },
-                    'FileUploaded': function (up, file, info) {
+                    'FileUploaded': (up, file, info)=>{
                         uploadStatus.textContent = '上传完毕'
                         window.eventHub.emit('afterUpload')
-
+                        this.model.data.status='open'
                         // 每个文件上传成功后,处理相关的事情
                         // 其中 info.response 是文件上传成功后，服务端返回的json，形式如
                         // {
